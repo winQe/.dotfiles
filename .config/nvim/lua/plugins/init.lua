a4919ce9b852c820b8e91b6f3e4b70db02990211
Adin <andhika.bhayangkara@desaysv.com>
2024-06-06 14:24:50 +0800

    Added lazygit in nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -32,16 +32,39 @@ return {
       },
     },
   },
+
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require "lua.configs.gitsigns"
     end,
   },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
   {
     "ThePrimeagen/vim-be-good",
     cmd = { "VimBeGood" },
   },
+
   {
     "windwp/nvim-ts-autotag",
     ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
@@ -49,6 +72,7 @@ return {
       require("nvim-ts-autotag").setup()
     end,
   },
+
   {
     "kawre/leetcode.nvim",
     build = ":TSUpdate html",
