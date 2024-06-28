78601a554a5b88fdf4a2c9368c623904f9c1ecca
Adin <andhika.bhayangkara@desaysv.com>
2024-06-28 13:05:08 +0800

    Add codesnap.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -120,4 +120,17 @@ return {
     end,
     lazy = false,
   },
+  {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    keys = {
+      { "<leader>pc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+      { "<leader>ps", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+    },
+    opts = {
+      save_path = "~/Pictures",
+      has_breadcrumbs = true,
+      bg_theme = "grape",
+    },
+  },
 }
