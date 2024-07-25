cbc6533fa23a04ddd1b2b4d4d2ef2f539793c9d5
Adin <andhika.bhayangkara@desaysv.com>
2024-07-25 13:50:36 +0800

    Added .clang-tidy

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -30,6 +30,7 @@ lspconfig.clangd.setup {
   cmd = {
     -- see clangd --help-hidden
     "clangd",
+    "--background-index",
     -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
     -- to add more checks, create .clang-tidy file in the root directory
     -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
