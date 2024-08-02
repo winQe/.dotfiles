ef5ea898a2488c1b1c78c0f8d39eeb2610be953d
Adin <andhika.bhayangkara@desaysv.com>
2024-08-02 10:22:53 +0800

    Updated flash configs

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -180,7 +180,17 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+      jump = {
+        autojump = true,
+      },
+      modes = {
+        search = { enabled = true },
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
   -- stylua: ignore
   keys = {
     { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
