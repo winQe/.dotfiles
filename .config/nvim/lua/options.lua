de4e2140b45889ea3ace8c47fae9e6ec2140d225
Adin <adinbhayangkara@gmail.com>
2024-08-10 22:10:43 +0800

    Updated extensions color

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,6 +6,11 @@ local o = vim.o
 o.relativenumber = true -- default is relative line number
 o.scrolloff = 5 -- minimum number of lines to keep above and below the cursor
 o.conceallevel = 1
+o.termguicolors = true
+
+-- Set colors for extensions
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", { fg = "#89dceb" })
+vim.api.nvim_set_hl(0, "FlashLabel", { bg = "#eba0ac", fg = "#11111b" })
 
 vim.cmd "Hardtime enable" -- enable Hardtime nvim
 -- o.cursorlineopt ='both' -- to enable cursorline!
