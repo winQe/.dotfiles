1c01225b942ef59568da0611b9062c6ea6ba7ee1
Adin <adinbhayangkara@gmail.com>
2024-08-10 21:58:49 +0800

    Set conceal level 1

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -5,6 +5,7 @@ require "nvchad.options"
 local o = vim.o
 o.relativenumber = true -- default is relative line number
 o.scrolloff = 5 -- minimum number of lines to keep above and below the cursor
+o.conceallevel = 1
 
 vim.cmd "Hardtime enable" -- enable Hardtime nvim
 -- o.cursorlineopt ='both' -- to enable cursorline!
