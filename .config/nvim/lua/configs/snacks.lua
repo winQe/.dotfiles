995b61036e92874916421635d8ee81c3667ce0cc
Adin <andhika.bhayangkara@desaysv.com>
2024-11-21 13:55:22 +0800

    Improved dashboard

--- .config/nvim/lua/configs/snacks.lua
+++ .config/nvim/lua/configs/snacks.lua
@@ -2,7 +2,26 @@ return {
   bigfile = { enabled = true },
   notifier = { enabled = true },
   quickfile = { enabled = true },
-  statuscolumn = { enabled = true },
   words = { enabled = true },
-  dashboard = { enabled = true }
+  dashboard = {
+    enabled = true,
+    sections = {
+      { section = "header" },
+      { section = "keys", gap = 1, padding = 1 },
+      { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+      { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+      {
+        pane = 2,
+        icon = " ",
+        title = "Git Status",
+        section = "terminal",
+        cmd = "hub status --short --branch --renames",
+        height = 5,
+        padding = 1,
+        ttl = 5 * 60,
+        indent = 3,
+      },
+      { section = "startup" },
+    },
+  }
 }
