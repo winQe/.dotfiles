9de1d7b53f45c95d9264ea6bdba093fc76520bbb
Adin <andhika.bhayangkara@desaysv.com>
2025-08-11 17:45:13 +0800

    Updated obsidian file creation function

--- .config/nvim/lua/configs/obsidian.lua
+++ .config/nvim/lua/configs/obsidian.lua
@@ -1,6 +1,7 @@
 return {
   completion = {
     blink = true,
+    min_chars = 0,
   },
   workspaces = {
     {
@@ -28,4 +29,24 @@ return {
   ui = {
     enabled = false,
   },
+  -- Optional, customize how note IDs are generated given an optional title.
+  ---@param title string|?
+  ---@return string
+  note_id_func = function(title)
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
+    -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return suffix
+  end,
 }
