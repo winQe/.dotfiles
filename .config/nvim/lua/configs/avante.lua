7d4a95ff9c9ff33e411cf8a7cf710de6ce03ae5e
Adin <adinbhayangkara@gmail.com>
2025-10-12 13:56:03 +0800

    Refactored avante configs

---
+++ .config/nvim/lua/configs/avante.lua
@@ -0,0 +1,26 @@
+return {
+  -- this file can contain specific instructions for your project
+  instructions_file = "avante.md",
+  -- for example
+  provider = "claude",
+  providers = {
+    claude = {
+      endpoint = "sec.ret.com",
+      model = "claude-sonnet-4-5",
+      timeout = 30000, -- Timeout in milliseconds
+      extra_request_body = {
+        temperature = 0.75,
+        max_tokens = 64000,
+      },
+    },
+    moonshot = {
+      endpoint = "https://api.moonshot.ai/v1",
+      model = "kimi-k2-0711-preview",
+      timeout = 30000, -- Timeout in milliseconds
+      extra_request_body = {
+        temperature = 0.75,
+        max_tokens = 32768,
+      },
+    },
+  },
+}
