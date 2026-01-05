1969b71c2be006b09e87a1d0e011a75351e46a14
Adin <adinbhayangkara@gmail.com>
2026-01-05 11:41:52 +0800

    fix: readded visual and substitute hl in chadrc

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,6 +6,11 @@ local M = {}
 
 M.base46 = {
   theme = "catppuccin",
+
+  hl_override = {
+    ["Visual"] = { bg = "#dd7878", fg = "black" },
+    ["Substitute"] = { bg = "#f38ba8", fg = "#1e1e2e" },
+  },
 }
 
 return M
