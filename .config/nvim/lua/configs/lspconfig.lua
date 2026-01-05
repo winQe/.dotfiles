a3d885bd599f103f30eb74899d42efd60efc16fa
Adin <adinbhayangkara@gmail.com>
2026-01-05 11:25:36 +0800

    feat: update lspconfig to built in nvim v0.11 built in lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,30 +1,24 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
--- Disabled global diagnostic
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = false,
-})
+-- Disabled global diagnostic virtual text
+vim.diagnostic.config({ virtual_text = false })
 
-local lspconfig = require "lspconfig"
-local util = require "lspconfig/util"
-local servers = { "html", "cssls", "eslint", "pyright", "terraformls" }
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
-
-lspconfig.ts_ls.setup {
-  on_attach = on_attach,
+-- Base config for all servers
+vim.lsp.config("*", {
   on_init = on_init,
   capabilities = capabilities,
+})
+
+-- Simple servers with default config
+local servers = { "html", "cssls", "eslint", "pyright", "terraformls", "sourcekit" }
+
+for _, server in ipairs(servers) do
+  vim.lsp.config(server, {})
+end
+
+vim.lsp.config("ts_ls", {
   settings = {
     typescript = {
       inlayHints = {
@@ -38,30 +32,20 @@ lspconfig.ts_ls.setup {
       },
     },
   },
-}
+})
 
-lspconfig.clangd.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+vim.lsp.config("clangd", {
   filetypes = { "cpp", "c", "cxx", "h", "hpp" },
   cmd = {
-    -- see clangd --help-hidden
     "clangd",
     "--query-driver=/usr/bin/c++,/usr/bin/g++,/usr/bin/gcc",
     "--background-index",
-    -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
-    -- to add more checks, create .clang-tidy file in the root directory
-    -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
     "--clang-tidy",
   },
-}
+})
 
-lspconfig.rust_analyzer.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  root_dir = util.root_pattern "Cargo.toml",
+vim.lsp.config("rust_analyzer", {
+  root_markers = { "Cargo.toml" },
   settings = {
     ["rust-analyzer"] = {
       cargo = {
@@ -69,14 +53,12 @@ lspconfig.rust_analyzer.setup {
       },
     },
   },
-}
+})
 
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
+vim.lsp.config("gopls", {
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  root_markers = { "go.work", "go.mod", ".git" },
   settings = {
     gopls = {
       completeUnimported = true,
@@ -106,10 +88,8 @@ lspconfig.gopls.setup {
       },
     },
   },
-}
+})
 
-lspconfig.sourcekit.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+-- Enable all configured servers
+vim.lsp.enable(servers)
+vim.lsp.enable { "ts_ls", "clangd", "rust_analyzer", "gopls" }
